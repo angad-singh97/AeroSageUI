3074fff54225710855da4dcd7322a864c2bcb00b
Angad Singh <gatecrash360@gmail.com>
2023-04-08 22:39:52 +0530

    AERO-001: App Skeleton
    - 'greatly' improved the searchbar UI
    - integrations for autopopulate and fetch results are ongoing

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,18 +1,34 @@
 import React, {useState} from 'react';
+import './searchbar.css';
 
 
 const SearchBar = () => {
-    return (//define JSX here
-        <div>
-            <form action="something">
-                <input type="text" placeholder="Start.." name="search"/>
-                    <input type="text" placeholder="End.." name="search"/>
-                        <button type="submit">
-                            <i className="fa fa-search"></i>
-                        </button>
-            </form>
+
+    const [source, setSource] = useState('');
+    const [destination, setDestination] = useState('');
+
+    const handleSwap = () => {
+        const temp = source;
+        setSource(destination);
+        setDestination(temp);
+    };
+
+    const handleSearch = () => {
+        // Perform search based on source and destination values
+    };
+
+    return (
+        <div className="container">
+            <div className="form">
+                <label>Source:</label>
+                <input type="text" value={source} onChange={(e) => setSource(e.target.value)} />
+                <label>Destination:</label>
+                <input type="text" value={destination} onChange={(e) => setDestination(e.target.value)} />
+                <button onClick={handleSwap}>Swap</button>
+                <button onClick={handleSearch}>Search</button>
+            </div>
         </div>
-        );
+    );
 };
 
 export default SearchBar;
