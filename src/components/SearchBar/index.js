eb04d760aa37214d067ea8170af8ee5bb50b0939
Angad Singh <gatecrash360@gmail.com>
2023-04-16 23:47:16 +0530

    AERO-001: App Skeleton
    - cleaned up search UI and worked on populating search result entries
    - richer filtering options needed

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -47,6 +47,42 @@ const SearchBar = () => {
         // Perform search based on source and destination values
         console.log("Source = " + source)
         console.log("Destination = " + destination)
+
+
+        const searchResults = document.getElementById('search-results');
+
+        // Assuming the data from the backend is an array of objects with properties like "flightNumber", "sourceAirport", "destinationAirport", "departureTime", "arrivalTime", "duration", etc.
+        const data = [  {    flightNumber: "BA123",    sourceAirport: "LHR",    destinationAirport: "JFK",    departureTime: "2022-04-01T07:00:00Z",    arrivalTime: "2022-04-01T12:00:00Z",    duration: 300  },  {    flightNumber: "AF456",    sourceAirport: "CDG",    destinationAirport: "LAX",    departureTime: "2022-04-01T08:00:00Z",    arrivalTime: "2022-04-01T15:00:00Z",    duration: 420  }];
+
+        // Create the HTML table
+        const table = document.createElement('table');
+        const headerRow = table.insertRow();
+        const headers = ['Flight Number', 'Source Airport', 'Destination Airport', 'Departure Time', 'Arrival Time', 'Duration'];
+        headers.forEach(header => {
+            const th = document.createElement('th');
+            th.textContent = header;
+            headerRow.appendChild(th);
+        });
+
+        data.forEach(flight => {
+            const row = table.insertRow();
+            const flightNumber = row.insertCell();
+            flightNumber.textContent = flight.flightNumber;
+            const sourceAirport = row.insertCell();
+            sourceAirport.textContent = flight.sourceAirport;
+            const destinationAirport = row.insertCell();
+            destinationAirport.textContent = flight.destinationAirport;
+            const departureTime = row.insertCell();
+            departureTime.textContent = new Date(flight.departureTime).toLocaleString();
+            const arrivalTime = row.insertCell();
+            arrivalTime.textContent = new Date(flight.arrivalTime).toLocaleString();
+            const duration = row.insertCell();
+            duration.textContent = flight.duration;
+        });
+
+        // Add the table to the search results container
+        searchResults.innerHTML = '';
+        searchResults.appendChild(table);
     };
 
     return (
@@ -76,6 +112,9 @@ const SearchBar = () => {
                 </div>
 
             <button onClick={handleSearch}>Search</button>
+            </div>
+            <div className="results" id="search-results">
+                <p><i>Flight Search Results Show Here</i></p>
             </div>
         </div>
     );
